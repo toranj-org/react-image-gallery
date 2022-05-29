7c2a81a28b6d3bf6a028fadce4db4b33519eb3d2
toranj-org <omidpoorali1992@gmail.com>
2022-05-30 01:06:06 +0430

    style 404 page

--- src/module-system/views/not-found.jsx
+++ src/module-system/views/not-found.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import * as Home from '../../module-home';
 
-
 const NotFound = () => (
-    <div>
-        <h1>404</h1>
-        <p>Page not found</p>
-        <Link to={Home.Routes.Index.path}>Home</Link>
-    </div>
+    <section className="page-404-root">
+        <div className="page-404-container">
+            <h1 className="page-404-header">404</h1>
+            <img className="page-404-image" src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" alt="" />
+            <div className="page-404-footer">
+                <h3 className="h2">Look like you're lost</h3>
+                <p>the page you are looking for not avaible!</p>
+                <Link to={Home.Routes.Index.path}>Go to Home</Link>
+            </div>
+        </div>
+    </section>
 );
 
 export default NotFound;
